b8e11f90b0937005ef4bfbd0799b62efe0adfae7
LiJinChang <lijinchang2012@gmail.com>
2021-01-13 08:04:22 +0800

    Add files via upload

--- goormv2.sh
+++ goormv2.sh
@@ -3,13 +3,13 @@
 mkdir /v2ray
 cd /v2ray
 rm -f /v2ray/v2ray
-wget https://github.com/LiJinChang/goorm-v2ray/raw/master/v2ray
+wget https://github.com/LiJinChang/goorm-v2ray/raw/main/v2ray
 #设置运行权限
 chmod +x /v2ray/v2ray
 rm -f /v2ray/v2ctl
-wget https://github.comLiJinChang/goorm-v2ray/raw/master/v2ctl
+wget https://github.comLiJinChang/goorm-v2ray/raw/main/v2ctl
 chmod +x /v2ray/v2ctl
 rm -f /v2ray/config.json
-wget https://github.com/LiJinChang/goorm-v2ray/raw/master/config.json
+wget https://github.com/LiJinChang/goorm-v2ray/raw/main/config.json
 # 开始后台运行
 nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
