9d53331efd9fa3d17f6d092f6c8caf47baa91ef7
LiJinChang <lijinchang@westkorea.onmicrosoft.com>
2021-03-08 20:35:33 +0800

    Add files via upload

--- goormv2.sh
+++ goormv2.sh
@@ -2,14 +2,8 @@
 #下载核心程序
 mkdir /v2ray
 cd /v2ray
-rm -f /v2ray/v2ray
-wget https://github.com/LiJinChang/goorm-v2ray/raw/main/v2ray
-#设置运行权限
-chmod +x /v2ray/v2ray
-rm -f /v2ray/v2ctl
-wget https://github.com/LiJinChang/goorm-v2ray/raw/main/v2ctl
-chmod +x /v2ray/v2ctl
-rm -f /v2ray/config.json
+wget --no-check-certificate "https://github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip" && unzip -o v2ray-linux-64.zip v2ray v2ctl && rm v2ray-linux-64.zip
+chmod +x v2ray v2ctl
 wget https://raw.githubusercontent.com/LiJinChang/goorm-v2ray/main/config.json
 # 开始后台运行
 nohup /v2ray/v2ray -config=/v2ray/config.json >out.txt 2>&1 &
